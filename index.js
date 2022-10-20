75dea92b2e85fda7dabd171d60cdbb28999ee332
Sivaprakasharavi <sivaprakashravi@gmail.com>
2022-10-20 21:13:52 +0530

    bypass updated

--- index.js
+++ index.js
@@ -7,22 +7,37 @@ app.use(express.json());
 const port = 3000;
 
 app.post('/bypass', (req, res) => {
-    const {body} = req;
-    if (body?.url) {
-        axios.get(body?.url)
-            .then(data => {
-                data = JSON.parse(convert.xml2json(data, {compact: true, spaces: 2}));
-                res.send({data});
+    const { body } = req;
+    const { url, dataType, method, data } = body;
+    if (url) {
+        axios[method](url, data)
+            .then(({ data }) => {
+                if (dataType?.toLowerCase() === "xml" || typeof data === "string") {
+                    try {                        
+                        data = JSON.parse(convert.xml2json(data, { compact: true, spaces: 2 }));
+                    } catch(e) {
+                        res.send(e, 500);
+                    }
+                }
+                res.send(data);
             })
             .catch(err => {
                 console.log('Error: ', err.message);
-                res.send({err});
+                res.send(err, 500);
             });
     } else {
         res.send({
             err: "No URL to Bypass"
         });
     }
+});
+
+app.get('*', function(_req, res){
+    res.send('Not found!', 404);
+});
+
+app.post('*', function(_req, res){
+    res.send('Not found!', 404);
 });
 
 app.listen(port, () => {
